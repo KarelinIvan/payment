be675ea5832a75f4416f578bc65fbf6714746bc6
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-25 23:53:55 +0300

    12-й коммит

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -5,7 +6,8 @@ class Item(models.Model):
     """ Модель продукта """
     name = models.CharField(max_length=100, verbose_name='Наименование продукта',
                             help_text='Укажите наименование продукта')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', help_text='Укажите цену продукта')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', help_text='Укажите цену продукта',
+                                validators=[MinValueValidator(0)])
     description = models.TextField(verbose_name='Описание продукта', help_text='Укажите описание продукта')
 
     class Meta:
