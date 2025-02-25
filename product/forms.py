ec3916dd4faf93cc5b7464eff291d46b12c5efd5
karelin.ivan <ivan.karelin.1993@mail.ru>
2025-02-25 17:30:23 +0300

    4-й коммит

---
+++ product/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from product.models import Item
+
+class ItemForm(forms.ModelForm):
+    """ Форма добавления продукта """
+    class Meta:
+        model = Item
+        fields = ('name', 'price', 'description')
+
