0b421373094e32926b99fd6e2421c83e4a173e58
karelin.ivan <ivan.karelin.1993@mail.ru>
2025-02-25 17:32:01 +0300

    5-й коммит

--- product/views.py
+++ product/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from product.models import Item
+from product.forms import ItemForm
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+class PostListView(ListView):
+    """ Просмотр всех продуктов"""
+    model = Item
+    template_name = ''
+    context_object_name = 'products'
+
+
+class PostDetailView(DetailView):
+    """ Просмотр продукта """
+    model = Item
+    template_name = ''
+    context_object_name = 'product'
+
+
+class PostUpdateView(UpdateView):
+    """ Изменение продукта """
+    model = Item
+    form_class = ItemForm
+    template_name = ''
+    success_url = reverse_lazy('')
+
+
+class PostDeleteView(DeleteView):
+    """ Удаление продукта """
+    model = Item
+    template_name = ''
+    success_url = reverse_lazy('')
