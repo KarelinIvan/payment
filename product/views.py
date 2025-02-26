733d82847ac67c8382150879fe15ad4a3bbb9742
karelin.ivan <ivan.karelin.1993@mail.ru>
2025-02-26 11:24:58 +0300

    15-й коммит

--- product/views.py
+++ product/views.py
@@ -4,6 +4,9 @@ import stripe
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.cache import cache
 
 from config import settings
 
@@ -12,41 +15,50 @@ from product.models import Item
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-@api_view(['GET'])
-def get_item(request, pk):
-    """ Извлекает элемент из базы данных на основе предоставленного первичного ключа,
-    и отображает его с использованием связанного с ним HTML-шаблона """
-    try:
-        item = Item.objects.get(pk=pk)
-    except Item.DoesNotExist:
-        return JsonResponse({'error': 'Item not found'}, status=404)
-    return render(request, 'product/item.html', {'item': item})
+
+def get_item_from_cache_or_db(pk):
+    key = f'item_{pk}'
+    item = cache.get(key)
+    if not item:
+        try:
+            item = Item.objects.get(pk=pk)
+            cache.set(key, item, timeout=3600)  # Кэшируем на час
+        except Item.DoesNotExist:
+            pass
+    return item
+
+class GetItemView(APIView):
+    def get(self, request, pk):
+        item = get_item_from_cache_or_db(pk)
+        if not item:
+            return Response({'error': 'Item not found'}, status=404)
+        return render(request, 'product/item.html', {'item': item})
 
 
-@api_view(['GET'])
-def create_session(request, pk):
-    """ Создает сеанс оформления заказа Stripe для указанного продукта """
-    try:
-        item = Item.objects.get(pk=pk)
-    except Item.DoesNotExist:
-        return JsonResponse({'error': 'Item not found'}, status=404)
+class CreateSessionView(APIView):
+    def create_session(self, request, pk):
+        """ Создает сеанс оформления заказа Stripe для указанного продукта """
+        try:
+            item = Item.objects.get(pk=pk)
+        except Item.DoesNotExist:
+            return Response({'error': 'Item not found'}, status=404)
 
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[
-            {
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': item.name,
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': item.name,
+                        },
+                        'unit_amount': int(item.price * 100),
                     },
-                    'unit_amount': int(item.price * 100),
+                    'quantity': 1,
                 },
-                'quantity': 1,
-            },
-        ],
-        mode='payment',
-        success_url='http://example.com/product/success.html',
-        cancel_url='http://example.com/product/cancel.html',
-    )
-    return session.get('session_id'), session.get('success_url')
+            ],
+            mode='payment',
+            success_url='http://example.com/product/success.html',
+            cancel_url='http://example.com/product/cancel.html',
+        )
+        return session.get('session_id'), session.get('success_url')
